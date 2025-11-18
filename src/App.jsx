ba7ff24fc4c905c4c4d22ec2d0cbdc3a1b60a9f4
Mattia Zerbini <zermatty6@gmail.com>
2025-11-18 11:58:24 +0100

    Update navbar and carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
+import RestaurantNavbar from './components/RestaurantNavbar'
+import Home from './components/Home'
 
 function App() {
   return (
     <>
-    <Container>
-    <Row className="justify-content-center">
-    <div className="col col-12 col-md-8 col-lg-4">
-      <img src="https://placebear.com/300/300" alt="foto orso" className="w-100" />
-    <h1 className="text-center">CIAONE</h1>
-    </div>
-    </Row>
-      </Container>
-      </>
+    <RestaurantNavbar />
+    <Home />
+    </>
   )
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,39 @@
+import {Container, Row, Col, Carousel} from 'react-bootstrap'
+
+const Home = function(){
+    return(
+        <Container className="mt-3">
+    <Row className="justify-content-center">
+    {/*<Col className="col col-12 col-md-8 col-lg-4">*/}
+    <Col xs={12} md={8} lg={4}>
+         <Carousel>
+      <Carousel.Item>
+        <img src="https://placedog.net/300/300" className="w-100"/>
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src="https://placecats.com/300/300" className="w-100"/>
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src="https://placebear.com/300/300" className="w-100"/>
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    </Col>
+    </Row>
+      </Container>
+    )
+}
+export default Home
